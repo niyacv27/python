0d790c9661fcbfeeac049915d2d5cc7669527150
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-09-30 10:46:47 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+A=float(input("Enter the mark of 1st sub:"))
+a=float(input("Enter the mark of 2nd sub:"))
+B=float(input("Enter the mark of 3rd sub:"))
+b=float(input("Enter the mark of 4th sub:"))
+C=float(input("Enter the mark of 5 th sub:"))
+sum=A+a+B+b+C
+print("sum=" +str(sum))
+avg=sum/5
+print("avg of 5 subjects is "+ str(avg))
+            
+            
