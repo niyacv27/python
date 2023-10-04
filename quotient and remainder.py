b47ad75df55140b1fabcdfb70160d508bb663e4b
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-10-04 09:29:28 +0530

    program to read two numbers and prints their quotient and remainder

---
+++ quotient and remainder.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a)
+print(b)
+quotient=a//b
+remainder=a%b
+print("quotient=",quotient)
+print("remainder=",remainder)
