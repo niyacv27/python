42910971816514c4d6c5ccea6b58c905c1954b6b
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-10-04 10:01:03 +0530

    program to find lowest among three integers.

---
+++ lowest among three integers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+if a<b and a<c:
+    print(a,"is lowest")
+elif b<a and b<c:
+    print(b,"is lowest")
+elif c<a and c<b:
+    print(c,"is lowest")
+else:
+    print("numbers are equal")
