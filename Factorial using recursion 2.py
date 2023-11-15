269bd23789faf0e79a741f0d93f70284ca76e329
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-11-15 05:52:01 +0530

    Delete Factorial using recursion 2.py

--- Factorial using recursion 2.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter a number:"))
-def factorial(a):
-    if a<=1:
-        return 1
-    else:
-        return a*factorial(a-1)
-c=factorial(a)
-print("Factorial of "+str(a)+ " is:",c)
