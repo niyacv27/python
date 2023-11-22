44982cfb31a6271ea109b58632a3d062dddbd26f
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-11-22 10:55:03 +0530

    Delete program using try else.py

--- program using try else.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter a first  number:"))
-b=int(input("Enter second number:"))
-try:
-    c=a/b
-    print("Result:",c)
-except:
-        print("cannot be divisible by zero")
-else:
-        print("successfully completed")
